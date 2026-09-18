015e9cc111e33d9986cc23ecb19f8274b21d9b83
Maxym899 <maximschovk924@gmail.com>
2026-09-18 11:42:04 +0300

    Lab_1

--- Lab_1/Lab_1.cpp
+++ Lab_1/Lab_1.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+#include <cmath>
+#include <time.h>
+#include <string>
+
+using namespace std;
 
 int main()
 {
-	
+	string name;
+	cout << "ENter your name" << endl;
+	cin >> name;
+	cout << "Your name is: " << name;
+
 	return 0;
 }
